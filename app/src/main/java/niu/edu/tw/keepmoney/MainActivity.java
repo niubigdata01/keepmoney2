5415187755d2406e90b8953695c61efcef006ea0
Corgi <niubigdata01@gmail.com>
2018-03-21 00:00:08 +0800

    from-as

---
+++ app/src/main/java/niu/edu/tw/keepmoney/MainActivity.java
@@ -0,0 +1,14 @@
+package niu.edu.tw.keepmoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//ㄎㄎ
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
